f1f80639582c8160567e941b7fa5371ff89ebd5a
katgiadla <Kasia1997>
2021-01-20 00:46:06 +0100

    Add selection of item

--- src/mainView/Controller.java
+++ src/mainView/Controller.java
@@ -1,14 +1,14 @@
 package mainView;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
 import java.io.File;
-import java.util.Arrays;
 
 public class Controller {
 
@@ -47,6 +47,10 @@ public class Controller {
     }
 
     public void startAnalyse(ActionEvent event) {
+
+        WORD selectedItem = MenuWord.getSelectionModel().getSelectedItem();
+
+        //TODO Wybór odpowiedniego słowa
         //TODO Uruchomienie analizy
     }
 }
